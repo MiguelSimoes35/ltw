3a006d9e35b48223bf1fa31ee4e87ac452888489
antoniopedrodantas <pedro.dantascv@gmail.com>
2019-11-22 11:20:42 +0000

    added project starting  files

---
+++ proj/database/populate.sql
@@ -0,0 +1,3 @@
+--User
+--insert into User (username, password, name, email) values ('panda_lover32', '1234', 'Pedro', 'panda_lover32@gmail.com'),
+insert into User (username, password, name, email) values ('panda_lover33', '4321', 'Miguel', 'panda_lover33@gmail.com')
