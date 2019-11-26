f689208aef9067a69c78967b1894ef985c6b2c49
antoniopedrodantas <pedro.dantascv@gmail.com>
2019-11-26 18:47:51 +0000

    Connected to a database

---
+++ proj/html/process_sign_up.php
@@ -0,0 +1,24 @@
+<?php
+    include_once('../includes/include_database.php');
+
+    // variables
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $r_password = $_POST['repeat_password'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+
+    // access database
+    $db = Database::instance()->db();
+
+    // insert the new user
+    if($password == $r_password){
+        $stmt = $db->prepare('INSERT INTO User VALUES(?, ?, ?, ?)');
+        $stmt->execute(array($username, $password, $name, $email));         // IMPORTANTE - mudar a forma de guardar a password para encriptada
+        header('Location: initialPage.html');
+        exit;
+    }
+    
+
+
+?>
