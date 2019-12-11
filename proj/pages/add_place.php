32478f0d0793f8802289e9f678d3874d30cee7d3
antoniopedrodantas <pedro.dantascv@gmail.com>
2019-12-11 19:36:51 +0000

    Started adding place function

--- proj/pages/add_place.php
+++ proj/pages/add_place.php
@@ -1,3 +1,8 @@
 <?php
-    echo "WOHOOO!";
+    include_once('../templates/template_generic.php');
+    include_once('../templates/template_authentication.php');
+
+    template_header();
+    template_add_place();
+    template_footer();
 ?>
